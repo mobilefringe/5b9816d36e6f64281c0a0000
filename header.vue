ea0b82446033b34a0b5bbee15aeedbbf5c0d96fc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-12 15:50:48 -0400

    Malvern Town Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -171,12 +171,37 @@
                     'processedJobs'
                 ]),
                 searchList() {
-                  return _.union(
-                    this.processedStores,
-                    this.processedEvents,
-                    this.processedPromos,
-                    this.processedJobs
-                  );
+                  var events = this.processedEvents;
+                    _.forEach(events, function (value, key) {
+                        if (_.includes(value.eventable_type, 'Property')) {
+                            value.is_store = false;
+                        } else {
+                            value.is_store = true;    
+                        }
+                    });
+                    var promos = this.processedPromos;
+                    _.forEach(promos, function (value, key) {
+                        if (_.includes(value.promotionable_type, 'Property')) {
+                            value.is_store = false;
+                        } else {
+                            value.is_store = true;    
+                        }
+                    });
+                    var jobs = this.processedJobs;
+                    _.forEach(jobs, function (value, key) {
+                        if (_.includes(value.jobable_type, 'Property')) {
+                            value.is_store = false;
+                        } else {
+                            value.is_store = true;    
+                        }
+                    });
+                    var stores = this.processedStores;
+                    _.forEach(stores, function (value, key) {
+                        value.is_store = true;    
+                    });
+                    
+                    var list = _.union( stores, events, promos, jobs );
+                    return list;
                 },
                 locale: {
                     get () {
