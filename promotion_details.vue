8f65dd35e411d3eb2793e5c405f144a2f85de188
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-13 11:17:20 -0400

    Malvern Town Centre 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -88,8 +88,12 @@
                     if(this.currentPromo != null) {
                         if (this.currentPromo.promotionable_type === "Store"){
                             if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
-                                if(_.includes(this.currentPromo.store.store_front_url_abs, 'missing'))
-                                this.currentPromo.image_url = this.currentPromo.store.store_front_url_abs; 
+                                if(_.includes(this.currentPromo.store.store_front_url_abs, 'missing')){
+                                    
+                                }
+                                else {
+                                    this.currentPromo.image_url = this.currentPromo.store.store_front_url_abs; 
+                                }
                             }
                         } else {
                             if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
