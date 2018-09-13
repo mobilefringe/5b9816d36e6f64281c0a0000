17009dfb71a0f6cfa4f6389aaedbae9c7f6f21be
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-13 10:42:46 -0400

    Malvern Town Centre 1.0  /events.vue

---
+++ events.vue
@@ -0,0 +1,155 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+                    <div class="main_container">
+                        <h2>Events</h2>
+                    </div>
+                </div>
+                <div class="main_container mobile_padding margin_30">
+                    <div class="details_row">
+                        <div class="details_col_3 hidden_phone">
+                            <img class="img_max" src="http://via.placeholder.com/220x600" alt="" />    
+                        </div>
+                        <div class="details_col_9">
+                            <!-- EVENTS -->
+                            <b-card no-body class="mb-1 inside_page_toggle">
+                                <b-card-header header-tag="header" class="p-1" role="tab">
+                                    <b-btn block @click="toggleEvents = !toggleEvents" :aria-expanded="toggleEvents ? 'true' : 'false'" aria-controls="toggleEvents">
+                                        Events
+                                        <i v-if="toggleEvents"  class="fa fa-minus f"></i>
+                                        <i v-else  class="fa fa-plus"></i>
+                                    </b-btn>
+                                </b-card-header>
+                                <b-collapse v-if="eventList.length >= 1" v-for="event in eventList" v-model="toggleEvents" role="tabpanel" id="toggleEvents" class="accordion_body">
+                                    <b-card-body>
+                                        <div class="row">
+                                            <div class="col-md-5" v-if="">
+                                                <img :src="event.image_url" :alt="'Event: ' + event.name" class="max_img" />
+                                            </div>
+                                            <div class="col-md-7">
+                                                <h3 class="promo_name">{{event.name}}</h3>
+                                                <p class="promo_store_name">
+                                                    <router-link v-if="event.eventable_type == 'Store'" :to="'/stores/'+ event.store.slug">
+                                                        {{ event.store.name }}
+                                                    </router-link>
+                                                    <span v-else>{{ property.name }}</span>
+                                                    <span>| </span>
+                                                    <span v-if="isMultiDay(event)" class="promo_date">{{ event.start_date | moment("MMMM D", timezone)}} to {{ event.end_date | moment("MMMM D", timezone)}}</span>
+                                                    <span v-else class="promo_date">{{ event.start_date | moment("MMMM D", timezone)}}</span>
+                                                </p>
+                                                <div class="promo_desc" v-html="event.description_short"></div>
+                                                <router-link :to="'/events/'+ event.slug" class="hvr-icon-forward">
+						                            <i class="fa fa-caret-right hvr-icon"></i> <span class="read_more">View Event Details</span>
+				                                </router-link>
+                                            </div>
+                                        </div>
+                                        <hr class="promo_separator" />
+                                    </b-card-body>
+                                </b-collapse>
+                                <b-collapse v-if="eventList.length == 0" v-model="toggleEvents" role="tabpanel" id="toggleEvents" class="accordion_body">
+                                    <b-card-body>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <p>Sorry, there are no Events posted at this time. Please check back soon!</p>
+                                            </div>
+                                        </div>
+                                        <hr class="promo_separator" />
+                                    </b-card-body>
+                                </b-collapse>
+                            </b-card>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-lazy-load", "bootstrap-vue"], function (Vue, Vuex, moment, tz, VueMoment, VueLazyload, BootstrapVue) {
+        Vue.use(BootstrapVue);
+        Vue.use(VueLazyload);
+        return Vue.component("events-component", {
+            template: template, // the variable template will be injected,
+            data: function () {
+                return {
+                    dataLoaded: false,
+                    pageBanner: null,
+                    toggleEvents: false,
+                }
+            },
+            created (){
+                this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Events Banner');
+                    if(temp_repo !== null && temp_repo !== undefined) {
+                       temp_repo = temp_repo.images;
+                       this.pageBanner = temp_repo[0];
+                    }
+                    else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b9816d36e6f64281c0a0000/image/png/1531495616000/inside_banner.png"
+                        }
+                    }
+                    
+                    this.dataLoaded = true;
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'findRepoByName',
+                    'processedEvents',
+                ]),
+                eventList: function events() {
+                    var events = this.processedEvents;
+                    var showEvents = [];
+                    _.forEach(events, function (value, key) {
+                        var today = moment.tz(this.timezone).format();
+                        var showOnWebDate = moment.tz(value.show_on_web_date, this.timezone).format();
+                        if (today >= showOnWebDate) {
+                            if (value.store != null && value.store != undefined && _.includes(value.store.image_url, 'missing')) {
+                                value.store.image_url = site.default_logo_url;//"//codecloud.cdn.speedyrails.net/sites/5b9816d36e6f64281c0a0000/image/png/1531496511000/event placeholder.png";
+                            }
+                            
+                            if (_.includes(value.image_url, 'missing')) {
+                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5b9816d36e6f64281c0a0000/image/png/1531496511000/event placeholder.png";
+                            }
+                            
+                            value.description_short = _.truncate(value.description, { 'length': 100, 'separator': ' ' });
+                            
+                            showEvents.push(value);
+                        }
+                    });
+                    var sortedEvents = _.orderBy(showEvents, function (o) { return o.end_date });
+                    if (sortedEvents.length > 0) {
+                        this.toggleEvents = true;
+                    }
+                    return sortedEvents
+                }
+            },
+            methods: {
+                loadData: async function () {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "events")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                isMultiDay(promo) {
+                    var timezone = this.timezone
+                    var start_date = moment(promo.start_date).tz(timezone).format("MM-DD-YYYY")
+                    var end_date = moment(promo.end_date).tz(timezone).format("MM-DD-YYYY")
+                    if (start_date === end_date) {
+                        return false
+                    } else {
+                        return true
+                    }
+                }
+            }
+        });
+    });
+</script>
