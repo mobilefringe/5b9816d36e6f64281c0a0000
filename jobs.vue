589584cfb4fb9a3f70151f96b44026baab151fab
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:43:58 -0500

    Malvern Town Centre 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -82,7 +82,7 @@ define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue
             created() {
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Jobs Banner');
-                    if(temp_repo !== null && temp_repo !== undefined) {
+                    if(temp_repo  && temp_repo.images) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
                     }
