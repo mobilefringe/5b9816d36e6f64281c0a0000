3797c03c1cfb13ddedcff5aef91dbfffa7f34cf1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 10:54:05 -0500

    Malvern Town Centre 1.0  /leasing_temporary.vue

--- leasing_temporary.vue
+++ leasing_temporary.vue
@@ -128,7 +128,14 @@
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b9816d36e6f64281c0a0000/image/png/1531495616000/inside_banner.png"
                         }
                     }
-                    
+                    var temp_repo1 = this.findRepoByName('Leasing Side Banner');
+                    if(temp_repo1 != null) {
+                        this.sideBanner = temp_repo1.images[0];
+                    } else {
+                        this.sideBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b915e966e6f6472b6290000/image/png/1531495616000/inside_banner.png"
+                        }
+                    } 
                     if(response && response[0]) {
                         this.tempLeasing = response[0].data;
                     }
