b7e0b22d5435f56f7e0c324e14d5f38018dff8e6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:47:29 -0500

    Malvern Town Centre 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -83,7 +83,7 @@
             created() {
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Hours Banner');
-                    if(temp_repo  && temp_repo.images) {
+                    if(temp_repo && temp_repo.images) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
                     }
