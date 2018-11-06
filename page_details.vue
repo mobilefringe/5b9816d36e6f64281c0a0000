c792e8558d85f1d9d3384993c0180e7ffa8e259d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:01:11 -0500

    Malvern Town Centre 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -73,7 +73,7 @@
                                 _this.sideBanner = temp_repo1.images[0];
                             } else {
                                 _this.sideBanner = {
-                                    "image_url": "//codecloud.cdn.speedyrails.net/sites/5b915e966e6f6472b6290000/image/png/1531495616000/inside_banner.png"
+                                    "image_url": ""
                                 }
                             } 
                             _this.currentPage = response.data;
