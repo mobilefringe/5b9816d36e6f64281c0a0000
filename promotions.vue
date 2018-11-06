d1c8a4ca5bf8adda5f72f01dab759c75ef6d78ce
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:01:35 -0500

    Malvern Town Centre 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -96,13 +96,13 @@
                         }
                     }
                     var temp_repo1 = this.findRepoByName('Promotions Side Banner');
-                if(temp_repo1 != null) {
-                    this.sideBanner = temp_repo1.images[0];
-                } else {
-                    this.sideBanner = {
-                        "image_url": "//codecloud.cdn.speedyrails.net/sites/5b915e966e6f6472b6290000/image/png/1531495616000/inside_banner.png"
-                    }
-                } 
+                    if(temp_repo1 != null) {
+                        this.sideBanner = temp_repo1.images[0];
+                    } else {
+                        this.sideBanner = {
+                            "image_url": ""
+                        }
+                    } 
                     this.dataLoaded = true;
                 });
             },
