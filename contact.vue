5f3459cb3b5e43c2fe555d7868549e689a975e58
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:43:06 -0500

    Malvern Town Centre 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -92,7 +92,7 @@
             created() {
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Contact Us Banner');
-                    if(temp_repo !== null && temp_repo !== undefined) {
+                    if(temp_repo && temp_repo.images) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
                     }
