5c4572fc20f4fe8e9bebe6fcabf14ff4afcf416e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-04 14:49:03 -0500

    Malvern Town Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -14,7 +14,12 @@
                                     </div>
                                 </div>
                             </a>
-                            <div v-else class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                            <div v-else class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }">
+                                <div class="banner_text site_container" v-if="banner.name && banner.description">
+                                        <h2 class="banner_title">{{banner.name}}</h2>
+                                        <p class="banner_desc">{{banner.description}}</p>
+                                    </div>
+                            </div>
                         </div>
                     </slick>
                 </div>
