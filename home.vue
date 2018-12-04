d12abd6458e695eaa5b122a07f19c270a890321c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-04 14:48:47 -0500

    Malvern Town Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -4,10 +4,17 @@
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
                 <div class="home_banner_container">
-                    <slick ref="slick" :options="slickOptions"  v-if="homeBanners && homeBanners.length > 0">
+                    <slick ref="slick" :options="slickOptions" v-if="homeBanners && homeBanners.length > 0">
                         <div v-for="banner in homeBanners">
                             <a v-if="banner.url" :href="banner.url" class="">
-                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }">
+                                    <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }">
+                                    <div class="banner_text site_container" v-if="banner.name && banner.description">
+                                        <h2 class="banner_title">{{banner.name}}</h2>
+                                        <p class="banner_desc">{{banner.description}}</p>
+                                    </div>
+                                </div>
+                                </div>
                             </a>
                             <div v-else class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                         </div>
