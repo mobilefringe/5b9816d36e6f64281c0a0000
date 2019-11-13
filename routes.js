9cfde02c6490ecbe436db4f4e30e8a6010d40a58
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-13 00:11:48 -0500

    Malvern Town Centre 1.0  /routes.js

---
+++ routes.js
@@ -0,0 +1,250 @@
+define([], function () {
+    return [
+        {
+            path: '/',
+            name:'home',
+            component: view('home')
+        },
+        {
+            path: '/contact_us',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('contact'),
+                    meta: {
+                        pageName: 'Contact Us',
+                    },
+                    name: 'contact'
+                }
+            ]
+        },
+        {
+            path: '/events',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('events'),
+                    meta: {
+                        pageName: 'Events',
+                    },
+                    name: 'events'
+                },
+                {
+                    path: ':id',
+                    component: view('event_details'),
+                    meta: {
+                        pageName: 'Events',
+                    },
+                    name: 'eventDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/jobs',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('jobs'),
+                    meta: {
+                        pageName: 'Careers',
+                    },
+                    name: 'jobs'
+                },
+                {
+                    path: ':id',
+                    component: view('job_details'),
+                    meta: {
+                        pageName: 'Careers',
+                    },
+                    name: 'jobDetails',
+                    props: true
+                }
+            ]
+        },
+        // {
+        //     path: '/hours',
+        //     component: view('default'),
+        //     children: [
+        //         {
+        //             path: '',
+        //             component: view('hours'),
+        //             meta: {
+        //                 pageName: 'Hours',
+        //             },
+        //             name: 'hours'
+        //         }
+        //     ]
+        // },
+        {
+           path: '/hours',
+           component: view('default'),
+           children: [
+              {
+                 path: '',
+                 component: view('hours'),
+                 name: 'hourList',
+              }
+           ]
+        },
+        {
+            path: '/leasing',
+            component: view('default'),
+            children: [
+                {   
+                    path: '',
+                    component: view('leasing'),
+                    meta: {
+                        pageName: 'Leasing',
+                    },
+                    name: 'leasing'
+                },
+                {   
+                    path: '/leasing/permanent',
+                    component: view('leasing_permanent'),
+                    meta: {
+                        pageName: 'Permanent Leasing',
+                    },
+                    name: 'permanentLeasing'
+                },
+                {   
+                    path: '/leasing/specialty',
+                    component: view('leasing_temporary'),
+                    meta: {
+                        pageName: 'Specialty Leasing',
+                    },
+                    name: 'specialtyLeasing'
+                }
+            ]
+        },
+        {
+            path: '/map',
+            component: view('default'),
+            children: [
+                {   
+                    path: '',
+                    component: view('map'),
+                    meta: {
+                        pageName: 'Centre Map',
+                    },
+                    name: 'map'
+                }
+            ]
+        },
+        {
+            path: '/newsletter',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('newsletter'),
+                    meta: {
+                        pageName: 'Newsletter',
+                    },
+                    name: 'newsletter'
+                }
+            ]
+        },
+        {
+            path: '/pages',
+            component: view('default'),
+            children: [
+                {
+                    path: ':id',
+                    component: view('page_details'),
+                    name: 'pageDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/promotions',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('promotions'),
+                    meta: {
+                        pageName: 'Promotions',
+                    },
+                    name: 'promotions='
+                },
+                {
+                    path: ':id',
+                    component: view('promotion_details'),
+                    meta: {
+                        pageName: 'Promotions',
+                    },
+                    name: 'promotionDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/stores',
+            component: view('default'),
+            children: [
+                {   
+                    path: '',
+                    component: view('stores'),
+                    meta: {
+                        pageName: 'Store Directory',
+                    },
+                    name: 'storeList'
+                },
+                {
+                    path: ':id',
+                    component: view('store_details'),
+                    meta: {
+                        pageName: 'Store Details',
+                    },
+                    name: 'storeDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/thank_you',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('thank_you'),
+                    meta: {
+                        pageName: 'Thank You',
+                    },
+                    name: 'thankyou'
+                }
+            ]
+        },
+        {
+            path: '/search-results',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('search_results'),
+                    meta: {
+                        pageName: 'Search Results',
+                    },
+                    name: 'search-results'
+                }
+            ]
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+    ]
+
+    /*
+    * Asynchronously load view (lazy-loading)
+    * @param {string} name the filename (basename) of the view to load.
+    */
+    function view(name) {
+        return function(resolve) { require(['vue!' + name + '.vue'], resolve); }
+    };
+});
